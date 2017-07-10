a61824990d78a17abf99fcb1caf5ffd50b7b72ac
sajiur <sajuroki@gmail.com>
2017-07-10 07:40:31 -0700

    Add files via upload

---
+++ add.php
@@ -0,0 +1,19 @@
+<! DOCTYPE HTML >
+<html>
+<title></title>
+<head>
+<style>
+body{background:black;color:white;front-size:50px;}
+</style>
+</head>
+<body>
+<?php
+$x=4+6;
+echo $x;
+?>
+
+
+
+
+</body>
+</html>
